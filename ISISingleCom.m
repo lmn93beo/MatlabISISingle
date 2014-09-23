6507dde04006067dfbc9b694ebe7213465e05553
LMN <lmn93beo@yahoo.com>
2014-09-23 12:10:39 -0700

    Simplified code to exclude GUI

---
+++ ISISingleCom.m
@@ -0,0 +1,34 @@
+addpath(genpath('./'))
+
+global screenNum screenPTR 
+
+screenNum = 0;
+
+mode = 'CM';
+
+%Initialize stimulus parameter structures
+configurePstate(mode)
+configureMstate
+configureLstate
+
+
+% Open PTB Screen
+[screenPTR, windowRect] = Screen('OpenWindow', screenNum);
+
+makeSyncTexture
+
+%Make the buffer and play
+switch mode
+      case 'PG'
+            makeGratingTexture
+            playgrating
+      case 'CM'
+            makeRandomDots
+            playRandomDots
+      case 'LM'
+            playgrating_lumSquare            
+end
+            
+
+sca;
+clear all;
